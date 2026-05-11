a7d1784dd81f1124fbe981487ae15c8f58b93818
Akshayatejomurthula <akshayatejomurthula07@gmail.com>
2026-05-11 10:48:11 +0530

    Time: 311 ms (96.73%), Space: 19.4 MB (66.01%) - LeetHub

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumJumps(self, nums, target):
+        n = len(nums)
+
+        dp = [-1] * n
+        dp[0] = 0
+
+        for i in range(n):
+            if dp[i] == -1:
+                continue
+
+            ni = nums[i]
+
+            for j in range(i + 1, n):
+                if abs(nums[j] - ni) <= target:
+                    dp[j] = max(dp[j], dp[i] + 1)
+
+        return dp[-1]
